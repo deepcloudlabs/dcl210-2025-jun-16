6562deff4a7bd8320cbe202d6778393e3a1d470b
Binnur KURT <deepcloudlabs@gmail.com>
2025-06-17 15:29:00 +0300

    Update Exercise4.java

--- functional-programming/src/com/example/exercises/Exercise4.java
+++ functional-programming/src/com/example/exercises/Exercise4.java
@@ -1,8 +1,14 @@
 package com.example.exercises;
 
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.example.dao.CityDao;
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
+import com.example.domain.City;
+import com.example.domain.Country;
 
 /**
  * 
@@ -14,8 +20,14 @@ public class Exercise4 {
 	private static final CityDao cityDao = InMemoryWorldDao.getInstance();
 
 	public static void main(String[] args) {
-		// Find the highest populated capital city
-
+		countryDao.findAllCountries()
+		          .stream()
+		          .parallel()
+		          .mapToInt(Country::getCapital)
+		          .mapToObj(cityDao::findCityById)
+		          .filter(Objects::nonNull)
+		          .collect(Collectors.maxBy(Comparator.comparing(City::getPopulation)))
+		          .ifPresent(System.out::println);
 	}
 
 }
